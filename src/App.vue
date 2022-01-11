b59b633347ab47d3cad395d8f107f336caa1cc45
reinielpablo <reiniel.dan.pablo.@gmail.com>
2022-01-11 18:50:53 +0800

    Added for baru

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-container fluid id="app" class="centrify">
+    <AsOfTime />
+  </b-container>
+</template>
+
+<script>
+import AsOfTime from "@/components/AsOfTime";
+export default {
+  name: "App",
+  components: {
+    AsOfTime,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100vh;
+  width: 100vw;
+  margin: 0;
+  padding: 0;
+  /* background: rgb(198, 99, 244);*/
+  background: radial-gradient(
+    circle,
+    rgb(79, 147, 224) 0%,
+    rgb(0, 204, 255) 75%
+  );
+  /* background: yellow; */
+  overflow: hidden;
+}
+
+.centrify {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/components/AsOfTime.vue
@@ -0,0 +1,116 @@
+<template>
+  <div id="main" class="">
+    <b-row class="centrify">
+      <span style="font-size: 20px; margin-bottom: 20px" class="text"
+        >as of</span
+      >
+    </b-row>
+    <b-row class="centrify date">
+      <span>{{ today[0] }}</span>
+    </b-row>
+    <b-row class="centrify date">
+      <span>{{ today[1] }}</span>
+    </b-row>
+    <b-row class="centrify">
+      <span style="font-size: 20px; margin-top: 20px" class="text">{{
+        message
+      }}</span>
+    </b-row>
+    <b-row id="button" class="centrify">
+      <div id="iconbutton" @click="onClickHeart()">
+        <b-icon icon="heart-fill" id="icon"> </b-icon>
+      </div>
+    </b-row>
+  </div>
+</template>
+<script>
+let messages = [
+  "i miss you!",
+  "i miss you baruu!",
+  "wuv youuu!",
+  "wuv youuu baruuu!",
+  "i wuv youuu!",
+  "i wuv youuu baruu!",
+  "i want you baruu!",
+  "i want you!",
+  "i wanna kiss you mwahhh!",
+  "i wanna hug you!",
+  "gihigugma tikaa!",
+  "youre my baruuu!",
+  "youre my wuvvv!",
+  "gimingaw ko nimoo!",
+  "i gotchuu my wuv!",
+  "youre my favorite boy!",
+  "can't wait to see you!",
+  "my kokoro feels wavey when your name pops up!",
+  "youre my sunflower!",
+  "i want your warm hugss!",
+  "i think about u all the time my baruu!",
+];
+export default {
+  data() {
+    return {
+      today: null,
+      isVisible: true,
+      message: messages[Math.floor(Math.random() * messages.length)],
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      this.getTimeNow();
+      // this.isVisible = !this.isVisible;
+    }, 750);
+  },
+  methods: {
+    onClickHeart() {
+      this.message = messages[Math.floor(Math.random() * messages.length)];
+    },
+    getTimeNow() {
+      const today = new Date();
+      const date =
+        today.getFullYear() +
+        "/" +
+        (today.getMonth() + 1) +
+        "/" +
+        today.getDate();
+      const time =
+        today.getHours() + ":" + String(today.getMinutes()).padStart(2, "0");
+      this.secondsNow = today.getSeconds();
+      this.today = [date, time + " PHT"];
+    },
+  },
+  computed: {},
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Montserrat:900i&display=swap");
+
+#main {
+  font-size: 30px;
+  font-family: "Montserrat", sans-serif;
+}
+
+#iconbutton {
+  padding: 7px;
+  color: rgb(75, 25, 190);
+}
+
+.date {
+  font-size: 50px;
+  letter-spacing: 2px;
+  -webkit-text-fill-color: transparent;
+  -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-color: rgb(255, 255, 255);
+  text-shadow: 2.5px 2.5px #ff1f8f, 7px 7px #000000;
+}
+
+.text {
+  letter-spacing: 2px;
+  -webkit-text-fill-color: transparent;
+  -webkit-text-stroke-width: 0.5px;
+  -webkit-text-stroke-color: rgb(255, 255, 255);
+  text-shadow: 0px 0px #ff1f8f;
+  width: 90vw;
+}
+</style>
